32e2d1ab7731b8c622bfd1f7c69afbdf996d25ab
LABMAC12 <LABMAC12@imac-de-labmac12.local>
2019-02-01 20:13:31 -0600

    Initial Commit

---
+++ Unidad1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Unidad1
+//
+//  Created by LABMAC12 on 01/02/19.
+//  Copyright © 2019 LABMAC12. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

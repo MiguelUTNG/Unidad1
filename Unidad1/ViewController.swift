53a9acd14651882919776ef3328ff7421e58d7b4
Miguel Camacho <miguelangelcamachocampos@gmail.com>
2019-05-07 08:08:36 -0500

    Primer commit
    Primer commit

--- Unidad1/ViewController.swift
+++ Unidad1/ViewController.swift
@@ -9,7 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var radio1 : Double = 0.0
+    var radio2 : Double = 0.0
+    var altura : Double = 0.0
+    var resultado : Double = 0.0
+    var areaLateral: Double = 0;
+    let PI = 3.1416;
+    
+    @IBOutlet weak var txtRadio1: UITextField!
+    @IBOutlet weak var txtRadio2: UITextField!
+    @IBOutlet weak var txtAltura: UITextField!
+    @IBOutlet weak var txtResultado: UILabel!
+    
+    @IBAction func btnArea(_ sender: UIButton) {
+        radio1 = Double(txtRadio1.text!)!
+        radio2 = Double(txtRadio2.text!)!
+        altura = Double(txtAltura.text!)!
+        
+        areaLateral = PI*(radio1 + radio2) * altura;
+        
+        txtResultado.text = String(areaLateral)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
